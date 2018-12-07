540482eb8c1e67465208d76329e4f00b826da8a1
Yury Varlakov <yury@nopass.com>
2018-12-07 16:52:43 +0300

    Merge

--- ServerApp/ViewModel/MainGridViewModel.cs
+++ ServerApp/ViewModel/MainGridViewModel.cs
@@ -9,11 +9,6 @@ namespace ServerApp.ViewModel
 {
     public class MainGridViewModel : BindableBase
     {
-        public MainGridViewModel()
-        {
-            Text = "Hello text";
-        }
-
         private string _text;
         public string Text
         {
@@ -21,5 +16,64 @@ namespace ServerApp.ViewModel
             set { _text = value; }
         }
 
+
+
+        //private ServiceHost _host;
+        public List<UserData> _users { get; private set; }
+
+        public MainGridViewModel()
+        {
+            _users = new List<UserData>
+            {
+                new UserData
+                {
+                    Id = 1,
+                    FirstName = "test",
+                    LastName = "test",
+                    Online = true
+                },
+                new UserData
+                {
+                    Id = 2,
+                    FirstName = "test",
+                    LastName = "test",
+                    Online = true
+                },
+                new UserData
+                {
+                    Id = 3,
+                    FirstName = "test",
+                    LastName = "test",
+                    Online = true
+                }
+            };
+            //grid.ItemsSource = _users;
+            //_host = new ServiceHost(typeof(ServerConnectionService));
+            //_host.Open();
+        }
+
+        //private void GridMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+        //    var userData = grid.SelectedItem as UserData;
+        //    if (userData != null)
+        //    {
+        //        MessageBox.Show("ID: " + userData.Id + "\n Status: " + userData.Online
+        //            + "\n First Name: " + userData.FirstName + "\n Last Name: " + userData.LastName);
+        //    }
+        //}
+
+        public void AddUser(string firstName, string lastName)
+        {
+            var userData = new UserData
+            {
+                Id = _users.Count + 1,
+                FirstName = firstName,
+                LastName = lastName,
+                Online = true
+            };
+            _users.Add(userData);
+            //grid.Items.Refresh();
+            //var row = grid.ItemContainerGenerator.ContainerFromItem(userData);
+        }
     }
 }
